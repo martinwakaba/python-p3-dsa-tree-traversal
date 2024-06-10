59245017044c2efbc999dd0f2d88c6ae8ee42a81
MartinWakaba <martin.wakaba@student.moringaschool.com>
2024-06-10 11:13:05 +0300

    done

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,31 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        def dfs(node):
+            if node is None:
+                return None
+            if node['id'] == id:
+                return node
+            for child in node['children']:
+                result = dfs(child)
+                if result:
+                    return result
+            return None
+
+  
+        return dfs(self.root)
+
+    def get_element_by_id(self, id):
+        if self.root is None:
+            return None
+        queue = [self.root]
+
+        while queue:
+            node = queue.pop(0)
+            if node['id'] == id:
+                return node
+            queue.extend(node['children'])
+
+        return None
